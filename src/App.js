729b4d662d554be1dfec67d0e01414f665e94387
Jihtak <matteo.milanovic@gmail.com>
2020-01-14 04:23:40 +0100

    final version of monsters rolodex with styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card_list/card_list.component';
+import { SearchBox } from "./components/search-box/search-box.component";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			monsters: [],
+			searchField: ""
+		};
+	}
+
+	componentDidMount() {
+		//Lifecycle method componentDidMount calls the function inside it once it has rendered that component and executes it
+		fetch('https://jsonplaceholder.typicode.com/users') //We go to the designated address and grab the data from it
+			.then((response) => response.json()) //data gets to us formated in .json file that we can extract the datathat we need from
+			.then((users) => this.setState({ monsters: users })); //we take the data that we need and set them in our array( we change its state!!!) which we later call in render method
+	}
+
+	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter(monster =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		)
+		return (
+			<div className="App">
+				<h1>Monsters Rolodex</h1>
+				<SearchBox
+					placeholder="search monsters"
+					handleChange={e => this.setState({ searchField: e.target.value })}
+				/>
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
 }
 
 export default App;
